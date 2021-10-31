a52a2ddb2b229ca6674207b438c1847c261b9b38
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-10-31 14:59:29 +0600

    ECourier Module added

---
+++ modules/ecourier/controllers/admin/AdminECourierManifest.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * DHL Deutschepost
+ *
+ * @author    silbersaiten <info@silbersaiten.de>
+ * @copyright 2021 silbersaiten
+ * @license   See joined file licence.txt
+ * @category  Module
+ * @support   silbersaiten <support@silbersaiten.de>
+ * @version   1.0.7
+ * @link      http://www.silbersaiten.de
+ */
+
+class AdminECourierManifestController extends ModuleAdminController
+{
+    public function __construct()
+    {
+        $this->table = '';
+        $this->bootstrap = true;
+        $this->show_toolbar = false;
+        $this->multishop_context = Shop::CONTEXT_SHOP;
+        $this->context = Context::getContext();
+
+        parent::__construct();
+
+        $this->display = 'manifest';
+    }
+
+    public function initContent()
+    {
+        parent::initContent();
+        if (Shop::isFeatureActive() && Shop::getContext() != Shop::CONTEXT_SHOP) {
+            $this->displayInformation($this->l('You can only display the page in a shop context.'));
+        } else {
+            if ($this->display == 'manifest') {
+                $this->content .= $this->renderManifest();
+            }
+        }
+    }
+
+    public function initProcess()
+    {
+        parent::initProcess();
+        if (Tools::getIsset('manifest'.$this->table)) {
+            $this->display = 'manifest';
+            $this->action = 'manifest';
+        }
+    }
+
+    public function postProcess()
+    {
+        parent::postProcess();    
+    }
+
+    public function renderManifest()
+    {
+        $ecourier_orders = Db::getInstance()->executeS(
+            'SELECT * FROM '._DB_PREFIX_.'ecourier_order po 
+            INNER JOIN '._DB_PREFIX_.'orders odr ON (po.id_order=odr.id_order)
+            ORDER BY \'po.date_add\' DESC LIMIT 20
+            '
+            // INNER JOIN '._DB_PREFIX_.'order_detail od ON (po.id_order=od.id_order)
+            // LEFT JOIN '._DB_PREFIX_.'ecourier_order_tracking pot ON (po.id_ecourier_order=pot.id_ecourier_order)group by po.reference'
+        );
+        // var_dump($ecourier_orders);
+
+        foreach ($ecourier_orders as $key=>$order){
+            $_SQL = 'SELECT * FROM '._DB_PREFIX_.'ecourier_order_tracking where id_order ='.$order['id_order'];
+            $ecourier_orders[$key]['tracking_data'] = Db::getInstance()->executeS($_SQL);
+            $ORDER_DETAILS_SQL = 'SELECT * FROM '._DB_PREFIX_.'order_detail where id_order ='.$order['id_order'];
+            $ecourier_orders[$key]['products'] = Db::getInstance()->executeS($ORDER_DETAILS_SQL);
+        }
+
+        $this->context->smarty->assign([
+            'manifestDate' => Tools::getValue('manifestDate', date('Y-m-d')),
+            'orders' => $ecourier_orders,
+        ]);
+        $this->setTemplate('manifest.tpl');
+    }
+}
---
+++ modules/ecourier/views/js/admin_configure.js
@@ -0,0 +1,65 @@
+/**
+ * DHL Deutschepost
+ *
+ * @author    silbersaiten <info@silbersaiten.de>
+ * @copyright 2020 silbersaiten
+ * @license   See joined file licence.txt
+ * @category  Module
+ * @support   silbersaiten <support@silbersaiten.de>
+ * @version   1.0.0
+ * @link      http://www.silbersaiten.de
+ */
+
+var dhldp_admin_configure = {
+    init: function() {
+        var self = this;
+        // $('select.select2').select2({});
+        self.validateForm();
+    },
+    validateForm: function() {
+        $("#dhl_global_settings").validate({
+            rules: {
+                /*
+                "DHL_SBX_CIGUSER":{
+                    "required": {
+                        depends: function(element) {
+                            return $("#dhl_mode_sbx").is(":checked");
+                        }
+                    }
+                },
+                "DHL_SBX_CIGPASS": {
+                    "required": {
+                        depends: function(element) {
+                            return $("#dhl_mode_sbx").is(":checked");
+                        }
+                    }
+                }
+                */
+            },
+            submitHandler: function(form) {
+                //doAjaxLogin($('#redirect').val());
+                form.submit();
+            },
+            // override jquery validate plugin defaults for bootstrap 3
+            highlight: function(element) {
+                $(element).closest('.form-group').addClass('has-error');
+            },
+            unhighlight: function(element) {
+                $(element).closest('.form-group').removeClass('has-error');
+            },
+            errorElement: 'span',
+            errorClass: 'help-block',
+            errorPlacement: function(error, element) {
+                if(element.parent('.input-group').length) {
+                    error.insertAfter(element.parent());
+                } else {
+                    error.insertAfter(element);
+                }
+            }
+        });
+    }
+}
+
+$(function(){
+    dhldp_admin_configure.init();
+})
