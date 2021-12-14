3304d9674b330cd293a3e3a532ac1fcb16561024
Tech Asif <asif.letsgobd@gmail.com>
2021-12-14 17:39:12 +0600

    lg_usp is done

--- modules/lg_usp/views/js/scripts.js
+++ modules/lg_usp/views/js/scripts.js
@@ -1,22 +1,18 @@
-$(document).ready(function () {});
 
-var vsOpts = {
-  $slides: $(".v-slide"),
-  $list: $(".v-slides"),
-  duration: 1,
-  lineHeight: 20,
-};
-
-var vSlide = new TimelineMax({
-  paused: true,
-  delay: 8,
-  repeat: -1,
-});
-
-vsOpts.$slides.each(function (i) {
-  vSlide.to(vsOpts.$list, vsOpts.duration, {
-    y: i * -1 * vsOpts.lineHeight,
-    ease: Sine.easeInOut,
+$(function(){
+  $('.tlt').textillate({ 
+    
+     in : {
+          effect:'flash'
+         
+      },
+      out: {
+          effect: 'flipOutX'
+          
+      },
+      loop: true
   });
 });
-vSlide.play();
+
+
+
